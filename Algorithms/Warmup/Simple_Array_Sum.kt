f5be8a9c05445b61efedad956705ef665f3ccf7a
EthanShin <avoirom@gmail.com>
2018-02-06 00:13:20 +0900

    read input & print output

--- Algorithms/Warmup/Simple_Array_Sum.kt
+++ Algorithms/Warmup/Simple_Array_Sum.kt
@@ -2,5 +2,14 @@ import java.io.*
 import java.util.*
 
 fun main(args: Array<String>) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+    val integer: Int = Integer.valueOf(readLine()!!)    // warning: varable 'integer' is never used.
+    val input = readLine()!!.split(' ')
+    var sum = 0
+
+    for (i in input) {
+        sum += Integer.valueOf(i)
+    }
+
+    print("$sum")
 }
