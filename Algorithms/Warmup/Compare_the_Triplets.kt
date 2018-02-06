62832b6200563c35804e4cdba446cd3ff8b06921
EthanShin <avoirom@gmail.com>
2018-02-06 21:38:42 +0900

    Convert StringList to IntList

--- Algorithms/Warmup/Compare_the_Triplets.kt
+++ Algorithms/Warmup/Compare_the_Triplets.kt
@@ -3,8 +3,8 @@ import java.util.*
 
 fun main(args: Array<String>) {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
-    val alice = readLine()!!.split(' ')
-    val bob = readLine()!!.split(' ')
+    val alice = readLine()!!.split(' ').map { it.toInt() }
+    val bob = readLine()!!.split(' ').map { it.toInt() }
     
     var aliceScore = 0
     var bobScore = 0
