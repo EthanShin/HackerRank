61b7d5917990ec23e1b5959836fd2a7006a50fe7
EthanShin <avoirom@gmail.com>
2018-02-06 21:43:53 +0900

    Add a comparing elements part

--- Algorithms/Warmup/Compare_the_Triplets.kt
+++ Algorithms/Warmup/Compare_the_Triplets.kt
@@ -8,6 +8,14 @@ fun main(args: Array<String>) {
     
     var aliceScore = 0
     var bobScore = 0
+
+    for (i in alice.indices) {
+        if (alice.elementAt(i) > bob.elementAt(i)) {
+            aliceScore++
+        } else if (alice.elementAt(i) < bob.elementAt(i)) {
+            bobScore++
+        }
+    }
     
     print("$aliceScore $bobScore")
 }
