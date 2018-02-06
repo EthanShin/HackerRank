c5658ef6f4bba376a7252e08f83675ac5cb5ab48
EthanShin <avoirom@gmail.com>
2018-02-06 21:02:33 +0900

    read input & print output

--- Algorithms/Warmup/Compare_the_Triplets.kt
+++ Algorithms/Warmup/Compare_the_Triplets.kt
@@ -3,4 +3,11 @@ import java.util.*
 
 fun main(args: Array<String>) {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+    val alice = readLine()!!.split(' ')
+    val bob = readLine()!!.split(' ')
+    
+    var aliceScore = 0
+    var bobScore = 0
+    
+    print("$aliceScore $bobScore")
 }
