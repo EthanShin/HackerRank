cbc5b6bf135652062e2895637b05221979bdc88f
EthanShin <avoirom@gmail.com>
2018-02-08 00:44:56 +0900

    Use Long data type to calculrate large integers

--- Algorithms/Warmup/A_Very_Big_Sum.kt
+++ Algorithms/Warmup/A_Very_Big_Sum.kt
@@ -3,4 +3,13 @@ import java.util.*
 
 fun main(args: Array<String>) {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+    val integer: Int = Integer.valueOf(readLine()!!)
+    val integerList = readLine()!!.split(' ').map { it.toLong() }   // Long type
+    
+    var sum: Long = 0   //Long type: the resulting sum might exceed the above 32-bit integer range.
+    for (i in integerList) {
+        sum += i
+    }
+    
+    print(sum)
 }
