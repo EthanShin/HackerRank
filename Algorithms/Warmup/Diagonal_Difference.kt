3dea3328e2a75244ba9d35b39fe10f0a1c3c6c6c
EthanShin <avoirom@gmail.com>
2018-02-10 15:52:10 +0900

    read input

--- Algorithms/Warmup/Diagonal_Difference.kt
+++ Algorithms/Warmup/Diagonal_Difference.kt
@@ -3,4 +3,11 @@ import java.util.*
 
 fun main(args: Array<String>) {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+    val integer: Int = Integer.valueOf(readLine()!!)
+
+    var matrix = mutableListOf<List<Int>>()
+    for (i in 1..integer) {
+        val integerList = readLine()!!.split(' ').map { it.toInt() }
+        matrix.add(integerList)
+    }
 }
