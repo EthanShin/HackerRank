e3c671091e3b9eb4490212a3d37bf6d025d05220
EthanShin <avoirom@gmail.com>
2018-02-12 22:39:49 +0900

    Solved problem

--- Algorithms/Warmup/Diagonal_Difference.kt
+++ Algorithms/Warmup/Diagonal_Difference.kt
@@ -3,11 +3,18 @@ import java.util.*
 
 fun main(args: Array<String>) {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
-    val integer: Int = Integer.valueOf(readLine()!!)
+    val integer: Int = Integer.valueOf(readLine()!!) - 1
 
     var matrix = mutableListOf<List<Int>>()
-    for (i in 1..integer) {
+    for (i in 0..integer) {
         val integerList = readLine()!!.split(' ').map { it.toInt() }
         matrix.add(integerList)
     }
+
+    var sum = 0
+    for (i in 0..integer) {
+        sum += matrix[i][i] - matrix[i][integer-i]
+    }
+    
+    println(Math.abs(sum))
 }
