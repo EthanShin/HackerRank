6dc43c825060ea72fe10ae602424000b02308180
EthanShin <avoirom@gmail.com>
2018-02-25 23:38:29 +0900

    Modify simply

--- Algorithms/Warmup/Mini-Max_Sum.kt
+++ Algorithms/Warmup/Mini-Max_Sum.kt
@@ -4,25 +4,15 @@ import java.util.*
 fun main(args: Array<String>) {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
     val numbers = readLine()!!.split(' ').map { it.toLong() }
-    
+
     var minimal: Long
     var maximal: Long
-    
-    var min = numbers.first()
-    var max = numbers.first()
-    
-    for(i in numbers) {
-        if(i < min) {
-            min = i
-        }
-        
-        if(i > max) {
-            max = i
-        }
-    }
-    
-    minimal = numbers.sum() - max
-    maximal = numbers.sum() - min
+
+    val min = numbers.min()
+    val max = numbers.max()
+
+    minimal = numbers.sum() - max!!.toLong()
+    maximal = numbers.sum() - min!!.toLong()
 
     print("$minimal $maximal")
 }
