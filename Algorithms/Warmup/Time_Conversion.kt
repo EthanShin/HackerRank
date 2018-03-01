bb7c80ef2ff0104f55c89b8cc36eaa2b0de49a26
EthanShin <avoirom@gmail.com>
2018-03-01 23:58:53 +0900

    Use 'split

--- Algorithms/Warmup/Time_Conversion.kt
+++ Algorithms/Warmup/Time_Conversion.kt
@@ -4,11 +4,9 @@ import java.util.*
 fun main(args: Array<String>) {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
     val clock = readLine()!!
+    var clock2 = clock.split(':').map { }
 
     if(clock.contains("PM")) {
-        print("PM")
-    } else {
-        print("AM")
+        print(clock2[0])
     }
-
 }
