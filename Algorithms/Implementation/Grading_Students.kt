453defac6c1a0f4c0038bbdb41268a2979b33031
EthanShin <avoirom@gmail.com>
2018-03-02 23:18:37 +0900

    read input

---
+++ Algorithms/Implementation/Grading_Students.kt
@@ -0,0 +1,20 @@
+import java.io.*
+import java.util.*
+
+fun roundGrade(score: Int): Int {
+
+}
+
+fun main(args: Array<String>) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+    val sc = Scanner(System.`in`)
+    val num = sc.nextInt() - 1
+
+    var gradeList = mutableListOf<Int>()
+
+    for (i in 0..num) {
+        gradeList.add(sc.nextInt())
+    }
+
+    println(gradeList)
+}
