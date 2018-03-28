c10ae7a456fd02a3f527f988c14399dc4dae55c6
EthanShin <avoirom@gmail.com>
2018-03-28 10:51:32 +0900

    Solved problem

--- Algorithms/Implementation/Day_of_the_Programmer.kt
+++ Algorithms/Implementation/Day_of_the_Programmer.kt
@@ -1,27 +1,23 @@
 import java.io.*
 import java.util.*
 
-fun isLeapYear(year: Int): Boolean {
-    if (year < 1918 && year % 4 == 0) {
-        return true
+fun isLeapYear(year: Int, day: Int) {
+    if (year == 1918) {
+        println("26.09.1918")
+    } else if (year < 1918 && year % 4 == 0) {
+        println("12.09.$year")
+    } else if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        println("12.09.$year")
+    } else {
+        println("13.09.$year")
     }
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-        return true
-    }
-    return false
 }
 
 fun main(args: Array<String>) {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
     val sc = Scanner(System.`in`)
     val year = sc.nextInt()
-    val month = "09"
-    var day = 13
+    val day = 256
 
-    if (isLeapYear(year)) {
-        day = 12
-    }
-
-
-    println("$day.$month.$year")
+    isLeapYear(year, day)
 }
