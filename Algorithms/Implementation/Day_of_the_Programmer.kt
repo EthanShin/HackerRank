192eaf00d47e5341f01f43fcb7079e29025215a1
EthanShin <avoirom@gmail.com>
2018-03-28 10:24:28 +0900

    Add a condition statement in a function

--- Algorithms/Implementation/Day_of_the_Programmer.kt
+++ Algorithms/Implementation/Day_of_the_Programmer.kt
@@ -2,7 +2,10 @@ import java.io.*
 import java.util.*
 
 fun isLeapYear(year: Int): Boolean {
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+    if (year < 1918 && year % 4 == 0) {
+        return true
+    }
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
         return true
     }
     return false
