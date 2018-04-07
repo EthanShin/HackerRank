aafd09907443a434233616094d75646529903602
EthanShin <avoirom@gmail.com>
2018-04-07 15:42:20 +0900

    Solved problem

---
+++ Algorithms/Implementation/Electronics_Shop.kt
@@ -0,0 +1,39 @@
+import java.util.*
+
+fun getMoneySpent(keyboards: Array<Int>, drives: Array<Int>, b: Int): Int {
+
+    var selectableDevices = mutableListOf<Int>()
+
+    for (i in keyboards.filter { it -> it < b }) {
+        for (j in drives.filter { it -> it < b }) {
+            selectableDevices.add(i + j)
+        }
+    }
+
+    var result = selectableDevices.filter { it -> it <= b }.max()
+
+    result?.let {
+        return it
+    }
+    return -1
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val bnm = scan.nextLine().split(" ")
+
+    val b = bnm[0].trim().toInt()
+    val n = bnm[1].trim().toInt()
+    val m = bnm[2].trim().toInt()
+
+    val keyboards = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+    val drives = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    /*
+     * The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+     */
+
+    val moneySpent = getMoneySpent(keyboards, drives, b)
+
+    println(moneySpent)
+}
