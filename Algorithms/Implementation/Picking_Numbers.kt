b28fad137488c7557180426e466b9d14d0c81436
EthanShin <avoirom@gmail.com>
2018-04-23 23:12:34 +0900

    Solved problem

--- Algorithms/Implementation/Picking_Numbers.kt
+++ Algorithms/Implementation/Picking_Numbers.kt
@@ -2,9 +2,16 @@ import java.util.*
 
 fun getNumbers(numbers: List<Int>): Int {
 
-    println(numbers.groupingBy { it }.eachCount())
+    val mutableMap= numbers.groupingBy { it }.eachCount()
+    var result = 0
 
-    return -1
+    for (i in mutableMap.keys) {
+        var downValue = mutableMap.get(i-1)?.let { it } ?: 0
+        var sum = mutableMap.get(i)!! + downValue
+        if (sum > result) result = sum
+    }
+
+    return result
 }
 
 fun main(args: Array<String>) {
