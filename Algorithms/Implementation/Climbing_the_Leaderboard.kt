9f2877414e351fffc3088793084054cefc68c9b2
EthanShin <avoirom@gmail.com>
2018-04-24 18:37:51 +0900

    unfinished: timeout

--- Algorithms/Implementation/Climbing_the_Leaderboard.kt
+++ Algorithms/Implementation/Climbing_the_Leaderboard.kt
@@ -1,7 +1,30 @@
 import java.util.*
 
 fun climbingLeaderboard(scores: Array<Int>, alice: Array<Int>): Array<Int> {
+    var rankList = mutableListOf<Int>()
 
+    var setScores = scores.toSet().toTypedArray()
+    var rank = setScores.size + 1
+
+    for (i in alice) {
+
+        if (i >= setScores[0]) {
+            rankList.add(1)
+        } else {
+            for (j in (0..setScores.size - 1).reversed()) {
+
+                if (i >= setScores[j]) {
+                    rank = j + 1
+                } else {
+                    setScores = setScores.sliceArray(0..j)
+                    rankList.add(rank)
+                    break;
+                }
+            }
+        }
+    }
+
+    return rankList.toTypedArray()
 }
 
 fun main(args: Array<String>) {
