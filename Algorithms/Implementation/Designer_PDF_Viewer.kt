d6ec858e3ea4b744dbdd193b8d542fbd40b90ab3
EthanShin <avoirom@gmail.com>
2018-04-28 18:40:17 +0900

    Solved problem

--- Algorithms/Implementation/Designer_PDF_Viewer.kt
+++ Algorithms/Implementation/Designer_PDF_Viewer.kt
@@ -1,15 +1,24 @@
 import java.util.*
 
-fun PDFViewer(maximum: List<Int>, heights: String): Int {
-    return 0
+fun PDFViewer(heights: List<Int>, letters: String): Int {
+    var max = 0
+
+    for (c in letters) {
+        val height = heights.get(c.toInt() - 'a'.toInt())
+        if (height > max) {
+            max = height
+        }
+    }
+
+    return max * letters.length
 }
 
 fun main(args: Array<String>) {
     val scan = Scanner(System.`in`)
-    
+
     val heights = scan.nextLine().split(' ').map { it.toInt() }
     val letters = scan.nextLine()
-    
+
     val result = PDFViewer(heights, letters)
     println(result)
 }
