e4fb00c5d84e20aa0b6ee6449b911e7cc8454a8f
EthanShin <avoirom@gmail.com>
2018-04-30 23:24:49 +0900

    Solved problem

--- Algorithms/Implementation/Angry_Professor.kt
+++ Algorithms/Implementation/Angry_Professor.kt
@@ -5,19 +5,18 @@ fun angryProfessor(classInfo: List<String>, arriveTimes: List<String>): String {
     val students = classInfo[0].trim().toInt()
     val threshold = classInfo[1].trim().toInt()
 
-    if (arriveTimes.count { e -> e <= 0.toString() } < threshold) {
-        return "YES"
-    }
     return "NO"
 }
 
 fun main(args: Array<String>) {
 
     val scan = Scanner(System.`in`)
-    val testCases = scan.nextLine().trim().toInt()
+    val testCases = scan.nextInt()
 
     for (i in 1..testCases) {
         val classInfo = scan.nextLine().split(" ")
+
+
         val arriveTimes = scan.nextLine().split(" ")
 
         val result = angryProfessor(classInfo, arriveTimes)
