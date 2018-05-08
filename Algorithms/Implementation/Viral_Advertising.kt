96956c759e98b2483627c8658d70b09c70c6085d
EthanShin <avoirom@gmail.com>
2018-05-08 22:50:07 +0900

    Make a Frame

---
+++ Algorithms/Implementation/Viral_Advertising.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun viralAdvertising(basic_value: Int): Int {
+    return 0
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+    val days = scan.nextLine().trim().toInt()
+
+    val result = viralAdvertising(days)
+    println(result)
+}
