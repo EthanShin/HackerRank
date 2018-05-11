dfcbde37e178d1946e2592d08c80d40c5db79c0a
EthanShin <avoirom@gmail.com>
2018-05-11 23:24:35 +0900

    Solved problem

--- Algorithms/Implementation/Circular_Array_Rotation.kt
+++ Algorithms/Implementation/Circular_Array_Rotation.kt
@@ -7,11 +7,16 @@ fun main(args: Array<String>) {
     val n = firstLine[0].trim().toInt()
     val k = firstLine[1].trim().toInt()
     val q = firstLine[2].trim().toInt()
+    val rotation = k % n
 
     val secondLine = scan.nextLine().split(" ")
 
     for (i in 1..q) {
         val m = scan.nextLine().trim().toInt()
-        println(secondLine[(m + k - 1) % n])
+        if (m < rotation) {
+            println(secondLine[m + n - rotation])
+        } else {
+            println(secondLine[m - rotation])
+        }
     }
 }
