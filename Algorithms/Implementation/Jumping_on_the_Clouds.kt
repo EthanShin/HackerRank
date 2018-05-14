635e3042412a63b5f5c02d08bc5d24afeb0bf9c8
EthanShin <avoirom@gmail.com>
2018-05-14 23:49:08 +0900

    Solved problem

--- Algorithms/Implementation/Jumping_on_the_Clouds.kt
+++ Algorithms/Implementation/Jumping_on_the_Clouds.kt
@@ -3,21 +3,13 @@ import java.util.*
 fun jumpingOnClouds(c: Array<Int>, k: Int): Int {
 
     var i = 0
-    var energy = 100
-    while (true) {
-        energy -= 1
-        
+    var energy = 100 - c.size / k
+
+    while (i < c.size) {
         if (c[i] == 1) {
             energy -= 2
         }
         i += k
-
-        if (i == c.size) {
-            if (c[0] == 1) {
-                energy -= 2
-            }
-            break
-        }
     }
 
     return energy
