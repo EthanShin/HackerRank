491bd0c74bfb3224d1d73f781bd29e16aca9e948
EthanShin <avoirom@gmail.com>
2018-05-14 23:48:18 +0900

    unfinished: Wrong answer

--- Algorithms/Implementation/Jumping_on_the_Clouds.kt
+++ Algorithms/Implementation/Jumping_on_the_Clouds.kt
@@ -1,8 +1,26 @@
 import java.util.*
 
 fun jumpingOnClouds(c: Array<Int>, k: Int): Int {
-    
-    return 0
+
+    var i = 0
+    var energy = 100
+    while (true) {
+        energy -= 1
+        
+        if (c[i] == 1) {
+            energy -= 2
+        }
+        i += k
+
+        if (i == c.size) {
+            if (c[0] == 1) {
+                energy -= 2
+            }
+            break
+        }
+    }
+
+    return energy
 }
 
 fun main(args: Array<String>) {
