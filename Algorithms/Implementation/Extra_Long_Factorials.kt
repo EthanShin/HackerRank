3f62b49c0e6fd21a99cdf18e4f28b0314b38a965
EthanShin <avoirom@gmail.com>
2018-05-15 23:40:26 +0900

    Make a Frame

---
+++ Algorithms/Implementation/Extra_Long_Factorials.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun extraLongFactorials(n: Int): Unit {
+
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    extraLongFactorials(n)
+}
